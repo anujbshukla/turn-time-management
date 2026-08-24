f30d85985a201ad6df2f7239550e4b65ace14236
Anuj Shukla <anuj.b.shukla@hotmail.com>
2026-08-24 11:20:50 -0400

    Remove obsolete patch scripts and unused components

--- backend/app/engines/recovery_engine.py
+++
@@ -1,9 +0,0 @@
-class RecoveryEngine:
-    """
-    Operational Intelligence Engine.
-
-    Generates warehouse recovery plans.
-    """
-
-    def build_plan(self, appointment):
-        return None
